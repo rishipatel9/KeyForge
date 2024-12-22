a67748395acc8dc55c3d8c296da1f0f448b8478f
Rishi <rishipatel0826@gmail.com>
2024-12-22 22:54:30 +0530

    implement gRPC server with SayHello method and update go.mod for dependencies

--- main.go
+++ main.go
@@ -1,1 +1,35 @@
 package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	pb "KeyForge/grpc-gateway/pb"
+
+	"google.golang.org/grpc"
+)
+
+type server struct {
+	pb.UnimplementedGreeterServer
+}
+
+func (s *server) SayHello(ctx context.Context, req *pb.HelloRequest) (*pb.HelloResponse, error) {
+	log.Printf("Received: %v", req.GetName())
+	return &pb.HelloResponse{Message: "Hello " + req.GetName()}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
+
+	log.Println("Server is listening on port 50051...")
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
