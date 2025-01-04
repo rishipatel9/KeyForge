07a639faded55d123cc4d60e59b7856dc3acb4a8
Rishi <rishipatel0826@gmail.com>
2025-01-04 22:12:15 +0530

    add shard parsing functionality and implement tests for shard configuration

--- config/config.go
+++ config/config.go
@@ -1,6 +1,9 @@
 package config
 
 import (
+	"fmt"
+	"hash/fnv"
+
 	"github.com/BurntSushi/toml"
 )
 
@@ -26,4 +29,43 @@ type Shards struct {
 	Count   int
 	CurrIdx int
 	Addrs   map[int]string
+}
+
+func ParseShards(shards []Shard, currShardName string) (*Shards, error) {
+	shardCount := len(shards)
+	shardIdx := -1
+	addrs := make(map[int]string)
+
+	for _, s := range shards {
+		if _, ok := addrs[s.Idx]; ok {
+			return nil, fmt.Errorf("duplicate shard Index : %d", s.Idx)
+		}
+
+		addrs[s.Idx] = s.Address
+		if s.Name == currShardName {
+			shardIdx = s.Idx
+		}
+	}
+
+	for i := 0; i < shardCount; i++ {
+		if _, ok := addrs[i]; !ok {
+			return nil, fmt.Errorf("shard with index %d not found", i)
+		}
+	}
+
+	if shardIdx < 0 {
+		return nil, fmt.Errorf("shard with name %q was not found ", currShardName)
+	}
+
+	return &Shards{
+		Addrs:   addrs,
+		Count:   shardCount,
+		CurrIdx: shardIdx,
+	}, nil
+
+}
+func (s *Shards) Index(key string) int {
+	h := fnv.New64()
+	h.Write([]byte(key))
+	return int(h.Sum64() % uint64(s.Count))
 }
--- config/config_test.go
+++ config/config_test.go
@@ -9,17 +9,14 @@ import (
 	"testing"
 )
 
-func TestConfigParse(t *testing.T) {
-	contents := `[[shard]]
-	name="shard1"
-	idx=0
-	address="localhost:5000"
-	`
+func createConfig(t *testing.T, contents string) (config.Config, error) {
+	t.Helper()
 
 	f, err := ioutil.TempFile(os.TempDir(), "config.toml")
 
 	if err != nil {
 		log.Fatalf("Couldn't create temp file : %v", err)
+		return config.Config{}, err
 	}
 
 	defer f.Close()
@@ -29,11 +26,28 @@ func TestConfigParse(t *testing.T) {
 
 	if err != nil {
 		log.Fatalf("Could not write into the file : %v", err)
+		return config.Config{}, err
 	}
 
 	c, err := config.ParseFile(f.Name())
 	if err != nil {
 		log.Fatalf("Could not parse the file : %v", err)
+		return config.Config{}, err
+	}
+	return c, nil
+
+}
+func TestConfigParse(t *testing.T) {
+	contents := `[[shard]]
+		name="shard1"
+		idx=0
+		address="localhost:5000"
+	`
+
+	c, err := createConfig(t, contents)
+
+	if err != nil {
+		log.Fatalf("Error occured while creating config : %v", err)
 	}
 
 	want := config.Config{
@@ -46,8 +60,44 @@ func TestConfigParse(t *testing.T) {
 		},
 	}
 
-	if reflect.DeepEqual(c, want) {
+	if !reflect.DeepEqual(c, want) {
 		t.Errorf("The config does not match : %v and %v", c, want)
 	}
 
+}
+
+func TestParseShard(t *testing.T) {
+	c, err := createConfig(t, `
+	[[shard]]
+		name="shard1"
+		idx=0
+		address="localhost:5000"
+	[[shard]]
+		name="shard2"
+		idx=1
+		address="localhost:5001"
+	`)
+
+	if err != nil {
+		t.Fatalf("Error occurred while creating config: %v", err)
+	}
+
+	// Update currShardName to "shard2"
+	s, err := config.ParseShards(c.Shard, "shard2")
+	if err != nil {
+		t.Fatalf("Could not parse shards: %v", err)
+	}
+
+	want := &config.Shards{
+		Count:   2,
+		CurrIdx: 1,
+		Addrs: map[int]string{
+			0: "localhost:5000",
+			1: "localhost:5001",
+		},
+	}
+
+	if !reflect.DeepEqual(s, want) {
+		t.Errorf("Mismatch in shard config. Got: %+v, Want: %+v", s, want)
+	}
 }
