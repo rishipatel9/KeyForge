df4a0047a96553928aee5fedfbf3935a0a6c8d1b
Rishi <rishipatel0826@gmail.com>
2025-01-05 19:10:44 +0530

    add DeleteExtraKeys function and corresponding unit test

--- db/db.go
+++ db/db.go
@@ -63,3 +63,32 @@ func (d *Database) GetKey(key string) ([]byte, error) {
 	return nil, err
 
 }
+
+func (d *Database) DeleteExtraKeys(isExtra func(string) bool) error {
+	var keys []string
+	err := d.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket(defaultBucket)
+		return b.ForEach(func(k, v []byte) error {
+			KeyString := string(k)
+			if isExtra(KeyString) {
+				keys = append(keys, KeyString)
+			}
+			return nil
+		})
+	})
+	if err != nil {
+		return err
+	}
+
+	return d.db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket(defaultBucket)
+
+		for _, k := range keys {
+			if err := b.Delete([]byte(k)); err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+
+}
--- db/db_test.go
+++ db/db_test.go
@@ -3,12 +3,13 @@ package db_test
 import (
 	"KeyForge/db"
 	"bytes"
+	"fmt"
 	"io/ioutil"
 	"os"
 	"testing"
 )
 
-func TestGetSet(t *testing.T) {
+func TestDeleteExtraKeys(t *testing.T) {
 	f, err := ioutil.TempFile(os.TempDir(), "testdb")
 	if err != nil {
 		t.Fatalf("could not create temp file : %v", err)
@@ -24,16 +25,46 @@ func TestGetSet(t *testing.T) {
 		t.Fatalf("could not create database :%v", err)
 	}
 	defer closeFunc()
-	if err := db.SetKey("part", []byte("great")); err != nil {
-		t.Fatalf("count not write key to database : %v", err)
-	}
-	value, err := db.GetKey("party")
 
+	setKey(t, db, "party", "great")
+	setKey(t, db, "usa", "capitalistPigs")
+	// value := getKey(t, db, "party")
+
+	// if !bytes.Equal(value, []byte("great")) {
+	// 	t.Fatalf("unexpected value got key party : got  %q and want %q ", value, "great")
+	// }
+
+	if err := db.DeleteExtraKeys(func(name string) bool {
+		return name == "usa"
+	}); err != nil {
+		t.Fatalf("could not delete extra keys : %v", err)
+	}
+
+	if value := getKey(t, db, "party"); !bytes.Equal(value, []byte("great")) {
+		t.Fatalf("unexpected value got key party : got  %q and want %q ", value, "great")
+	}
+	if value := getKey(t, db, "us"); !bytes.Equal(value, []byte("")) {
+		t.Fatalf("unexpected value got key usa : got  %q and want %q ", value, "")
+	}
+
+}
+
+func setKey(t *testing.T, d *db.Database, key, value string) {
+	t.Helper()
+
+	if err := d.SetKey(key, []byte(value)); err != nil {
+		t.Fatalf("Set key failed : %v", err)
+	}
+	fmt.Printf("set the key %q to value %q successfully\n", key, value)
+}
+
+func getKey(t *testing.T, d *db.Database, key string) []byte {
+	t.Helper()
+	value, err := d.GetKey(key)
 	if err != nil {
-		t.Fatalf("could not get the key :%v", err)
+		t.Fatalf("Get key failed : %v", err)
+		return []byte("")
 	}
-	if bytes.Equal(value, []byte("great")) {
-		t.Fatalf("unexpected value got key party : got  %q and got %q ", value, "great")
-	}
+	return value
 
 }
