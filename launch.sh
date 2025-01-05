86c02f5a86b31a2faf438a887cf8d7e784a75c2a
Rishi <rishipatel0826@gmail.com>
2025-01-05 19:12:05 +0530

    add configuration for shard4 in launch script and sharding.toml

--- launch.sh
+++ launch.sh
@@ -12,5 +12,6 @@ go install -v
 KeyForge -db-location=shards/shard1.db -http-address=127.0.0.1:5000 -config-file=sharding.toml -shard=shard1 &
 KeyForge -db-location=shards/shard2.db -http-address=127.0.0.1:5001 -config-file=sharding.toml -shard=shard2 &
 KeyForge -db-location=shards/shard3.db -http-address=127.0.0.1:5002  -config-file=sharding.toml -shard=shard3 &
+KeyForge -db-location=shards/shard4.db -http-address=127.0.0.1:5003  -config-file=sharding.toml -shard=shard4 &
 
 wait
